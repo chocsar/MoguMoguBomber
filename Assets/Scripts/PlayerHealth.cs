71d20fe999d51aa67aba3dda307609c248ca9d14
片山凌 <ryotrumpet328@gmail.com>
2021-01-19 22:11:52 +0900

    PlayerHealthのりファクタ

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,16 +4,16 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float startingHealth = 100f;
-    public Slider slider;
-    public Image fillImage;
+    public float startHealth = 100f;
     public Color fullHealthColor = Color.green;
     public Color zeroHealthColor = Color.red;
-    public GameObject explosionPrefab;
 
-
+    [SerializeField] private Slider slider;
+    [SerializeField] private Image fillImage;
+    [SerializeField] private GameObject explosionPrefab;
     private AudioSource explosionAudio;
     private ParticleSystem explosionParticles;
+
     private float currentHealth;
     private bool dead;
 
@@ -21,21 +21,17 @@ public class PlayerHealth : MonoBehaviour
     private void Awake()
     {
         explosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
-
         explosionAudio = explosionParticles.GetComponent<AudioSource>();
-
         explosionParticles.gameObject.SetActive(false);
     }
 
-
     private void OnEnable()
     {
-        currentHealth = startingHealth;
+        currentHealth = startHealth;
         dead = false;
 
         SetHealthUI();
     }
-
 
     public void TakeDamage(float amount)
     {
@@ -49,26 +45,27 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-
     private void SetHealthUI()
     {
         slider.value = currentHealth;
 
-        fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, currentHealth / startingHealth);
+        //体力バーの色を変更する
+        fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, currentHealth / startHealth);
     }
-
 
     private void OnDeath()
     {
         dead = true;
 
+        //爆発エフェクト再生
         explosionParticles.transform.position = transform.position;
         explosionParticles.gameObject.SetActive(true);
-
         explosionParticles.Play();
 
+        //爆発サウンド再生
         explosionAudio.Play();
 
+        //プレイヤーを非アクティブにする
         gameObject.SetActive(false);
     }
 }
